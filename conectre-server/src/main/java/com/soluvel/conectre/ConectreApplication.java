972421f89ac906d95cdd4b6de5bb469f4cd89c89
soluvel <soluvel@tech.com>
2024-05-01 06:59:37 -0300

    Commit inicial com o backend

---
+++ conectre-server/src/main/java/com/soluvel/conectre/ConectreApplication.java
@@ -0,0 +1,13 @@
+package com.soluvel.conectre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectreApplication.class, args);
+	}
+
+}
---
+++ conectre-server/src/test/java/com/soluvel/conectre/ConectreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soluvel.conectre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
