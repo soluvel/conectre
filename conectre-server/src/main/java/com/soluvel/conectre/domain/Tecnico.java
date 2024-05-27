9c66e249d35ebc69cd82ae1f190a153aeed6a7ec
soluvel <soluvel@tech.com>
2024-05-27 07:37:01 -0300

    Refactor do front-end + funcionalidades sprint 4 + 5

--- conectre-server/src/main/java/com/soluvel/conectre/domain/Tecnico.java
+++ conectre-server/src/main/java/com/soluvel/conectre/domain/Tecnico.java
@@ -1,14 +1,10 @@
 package com.soluvel.conectre.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,4 +31,3 @@ public class Tecnico extends Usuario implements Serializable {
     @Embedded
     private Endereco endereco;
 }
- 
--- conectre-server/src/main/java/com/soluvel/conectre/repository/TecnicoRepository.java
+++ conectre-server/src/main/java/com/soluvel/conectre/repository/TecnicoRepository.java
@@ -2,7 +2,6 @@ package com.soluvel.conectre.repository;
 
 import com.soluvel.conectre.core.CrudRepository;
 import com.soluvel.conectre.domain.Tecnico;
-import com.soluvel.conectre.domain.UsuarioEmpresa;
 
 public interface TecnicoRepository extends CrudRepository<Tecnico, Long> {
 }
