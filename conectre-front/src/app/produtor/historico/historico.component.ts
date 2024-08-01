de5a28185918fb4079ba799f47b92ba61ea99688
Gabriel-Daantas <Gabrieldb2003@gmail.com>
2024-08-01 00:59:32 -0300

    Tela de histórico quase pronta

--- conectre-front/src/app/produtor/historico/historico.component.ts
+++ conectre-front/src/app/produtor/historico/historico.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HistoricoComponent {
 
+  tablePeixe: string[] = ['Volume de peixe', 'Nº de peixes por amostra', 'Mortalidade', 
+                          'Peso médio (Kg)', 'Biomassa total (Kg)', 'Ganho de peso (Kg)',
+                          'Ração', 'C.A (Conversão alimentar)', 'GPD (Ganho de peso diário)'];
+  
+  tableAmbiente: string[] = ['Data', 'Horário', 'Ph', 
+                          'Amônia', 'Nitrito', 'Alcalinidade',
+                          'Transparência', 'Temperatura', 'Oxigênio'];
+   
+
 }
