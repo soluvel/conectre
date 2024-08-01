032bbd2663e3e055c1290fc0805d8713eaf10ad4
Gabriel-Daantas <Gabrieldb2003@gmail.com>
2024-08-01 02:29:42 -0300

    Não lembro de quase nada do q arrumei nesse commit, estou com sono, mas foi bastante coisa

--- conectre-front/src/app/produtor/table-registro-historico/table-registro-historico.component.ts
+++ conectre-front/src/app/produtor/table-registro-historico/table-registro-historico.component.ts
@@ -69,6 +69,10 @@ export class TableRegistroHistoricoComponent implements OnInit {
     });
   }
 
+  search() {
+    
+  }
+
   redirectToDetails(id: any) {
     this.router.navigate(['/propriedade/editar', id]);
   }
