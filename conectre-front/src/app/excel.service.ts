c9f758075f5adaa29da6f58ee760893ebd1fd546
soluvel <soluvel@tech.com>
2024-09-10 08:55:29 -0300

    Ajustes gerais

--- conectre-front/src/app/excel.service.ts
+++ conectre-front/src/app/excel.service.ts
@@ -31,4 +31,13 @@ export class ExcelService {
     return this.http.get(`${this.apiUrl}/download-somatoria`, { headers, responseType: 'blob' });
   }
 
+  downloadPdf() {
+    const token = this.storage.getToken();
+    const headers = new HttpHeaders({
+      'Accept': 'application/octet-stream',
+      'Authorization': `${token}`
+    });
+    return this.http.get(`${this.apiUrl}/download-pdf`, { responseType: 'blob' });
+  }
+
 }
--- conectre-server/src/main/java/com/soluvel/conectre/controller/ExcelController.java
+++ conectre-server/src/main/java/com/soluvel/conectre/controller/ExcelController.java
@@ -5,6 +5,8 @@ import com.soluvel.conectre.repository.MedicaoRepository;
 import com.soluvel.conectre.service.ExcelService;
 import com.soluvel.conectre.service.MedicaoService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -51,5 +56,17 @@ public class ExcelController {
         headers.setContentDispositionFormData("attachment", "data.xlsx");
 
         return ResponseEntity.ok().headers(headers).body(excelContent);
+    }
+
+    @GetMapping("/download-pdf")
+    public ResponseEntity<Resource> downloadPdf() throws IOException {
+        String filePath = "C:/Users/kassi/Downloads/pdf-donwload.pdf";
+        Path path = Paths.get(filePath);
+        Resource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + path.getFileName().toString() + "\"")
+                .body(resource);
     }
 }
